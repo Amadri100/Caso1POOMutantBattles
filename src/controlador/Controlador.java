42b137f816c8c670f624488619deee374977bd84
Amadri <andresmadrigalcampos@gmail.com>
2026-09-23 12:41:22 -0600

    Solucion de errores

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -9,6 +9,7 @@ import modelo.Mutante;
 import juego.CampoDeBatalla;
 import juego.TipoEquipo;
 import  otros.Constantes;
+import controlador.HiloMutante;
 
 public class Controlador implements IObservable {
     private ArrayList<HiloMutante>[] listaHilosMutantes;
@@ -37,8 +38,8 @@ public class Controlador implements IObservable {
         for (int i = 0; i < cantidad; i++) {
             Mutante mutanteA = this.campoDeBatalla.getEquipoA().getListaMutantes().get(i);
             Mutante mutanteB = this.campoDeBatalla.getEquipoB().getListaMutantes().get(i);
-            this.listaHilosMutantes[TipoEquipo.EQUIPO_A.getIndice()].add(mutanteA);
-            this.listaHilosMutantes[TipoEquipo.EQUIPO_B.getIndice()].add(mutanteB);
+            this.listaHilosMutantes[TipoEquipo.EQUIPO_A.getIndice()].add(new HiloMutante(mutanteA));
+            this.listaHilosMutantes[TipoEquipo.EQUIPO_B.getIndice()].add(new HiloMutante(mutanteB));
         }
         
     }
