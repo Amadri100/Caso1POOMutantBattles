674c4db0def29f4620bd3c5b3cf2be0cc422a82a
David Zúniga <zpdavid71@gmail.com>
2026-09-23 12:45:56 -0600

    Se agregaron detalles en 'Controlador'

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -1,6 +1,5 @@
 package controlador;
 
-import controlador.HiloMutante;
 import java.io.Console;
 import java.util.ArrayList;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -10,6 +9,7 @@ import juego.CampoDeBatalla;
 import juego.TipoEquipo;
 import  otros.Constantes;
 import controlador.HiloMutante;
+import controlador.DatosJuego;
 
 public class Controlador implements IObservable {
     private ArrayList<HiloMutante>[] listaHilosMutantes;
@@ -45,7 +45,8 @@ public class Controlador implements IObservable {
     }
 
     public void obtenerDatosJuego() {
-        // Pendiente
+        // Pendiente agregar más detalles...
+        this.datosJuego = new DatosJuego(this);
     }
 
     @Override
